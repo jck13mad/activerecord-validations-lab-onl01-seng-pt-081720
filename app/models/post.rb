e76490853cf2be860b1db77fe18ff2f6360c2fa6
Jack Madden <jck13mad@gmail.com>
2021-01-21 15:24:23 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,7 +4,7 @@ class Post < ActiveRecord::Base
   validates :summary, length: { maximum: 250}
   validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
   
-  CLICKBAIT_PATTERNS = {
+  CLICKBAIT_PATTERNS = [
     
-  }
+  ]
 end
