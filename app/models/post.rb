1437bf2c4118e3a5f921591e72183e19e352ac5d
Jack Madden <jck13mad@gmail.com>
2021-01-21 15:26:14 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ActiveRecord::Base
   validates :content, length: { minimum: 250 }
   validates :summary, length: { maximum: 250}
   validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
-  validate :title_
+  validate :title_include
   
   CLICKBAIT_PATTERNS = [
     "Won't Believe",
@@ -11,4 +11,6 @@ class Post < ActiveRecord::Base
         "Top[number]",
         "Guess"
   ]
+  
+  def title_include
 end
